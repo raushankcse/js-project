036035ff022578b0387003e8e0ad545992c31792
Raushan Kumar <raushankcse@gmail.com>
2024-08-29 11:18:49 +0530

    project

---
+++ PriceRange/app.js
@@ -0,0 +1,14 @@
+let min = document.querySelector("#mini");
+const max = document.querySelector("#maxi");
+
+
+const range = document.querySelector("#rang")
+
+let rang = range.value;
+
+min.value = rang
+
+range 
+while(!range.max){
+    rang++;
+}
